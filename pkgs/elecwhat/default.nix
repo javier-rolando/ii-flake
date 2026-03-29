aa03612f8fffe546b43770bc1c38ffcbc7746b04
Javier Rolando <javier.rolando98@hotmail.com>
2026-03-29 22:50:58 +0000

    real hash

--- pkgs/elecwhat/default.nix
+++ pkgs/elecwhat/default.nix
@@ -12,7 +12,7 @@ buildNpmPackage {
     hash  = "sha256-jBSKnZ8q612jYOKe5rsccrcMpmPYQp9mKK64GyyqZCk=";
   };
 
-  npmDepsHash = lib.fakeHash;
+  npmDepsHash = "sha256-xWV4t5rwyUAMmHE9/TrrGBpSlixhnYD402WD19FvTgE=";
 
   # No usar electron-builder (usamos el electron de nixpkgs)
   # --ignore-scripts omite el postinstall "electron-builder install-app-deps"
