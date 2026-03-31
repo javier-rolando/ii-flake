76c6ab353f665095f381efc83995458a73a9fb3e
Javier Rolando <javier.rolando98@hotmail.com>
2026-03-31 12:40:50 -0300

    test

--- nixos-module.nix
+++ nixos-module.nix
@@ -26,8 +26,10 @@ in
     # ── Hyprland ──────────────────────────────────────────────────────────────
     programs.hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland; 
+
       portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+
       xwayland.enable = true;
     };
 
